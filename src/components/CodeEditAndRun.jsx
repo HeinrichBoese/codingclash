4a20fb4170ca873b4d81c6d26f1939efd03c4cd8
Tristan Kraus <tristankraus@me.com>
2020-06-25 12:57:10 +0200

    test running scaffold

--- src/components/CodeEditAndRun.jsx
+++ src/components/CodeEditAndRun.jsx
@@ -16,8 +16,8 @@ export default function CodeEditAndRun({ challenge }) {
   const [code, setCode] = useState(
     (challenge && challenge.template.replace(/\\n/g, "\n")) || ""
   );
-
   const [result, setResult] = useState(null);
+  const [testResults, setTestResults] = useState([]);
   const [error, setError] = useState(null);
   //const {currentUser} = useContext(AuthContext);
   const iframeRef = useRef(null);
@@ -33,7 +33,7 @@ export default function CodeEditAndRun({ challenge }) {
     return () => window.removeEventListener("message", handleMessage);
   }, []);
 
-  const calculate = () => {
+  const evaluate = () => {
     const codeEscaped = encodeURIComponent(code);
     iframeRef.current.srcdoc = `
       <script>
@@ -42,6 +42,28 @@ export default function CodeEditAndRun({ challenge }) {
       webworker.postMessage(\`${codeEscaped}\`);
       webworker.onmessage = (e) => window.parent.postMessage(e.data);
       </script>`;
+  };
+
+  const runTests = () => {
+    setTestResults([]);
+    function* genFunc() {
+      for (let item of challenge.testcases) {
+        yield item;
+      }
+    }
+    let genObj = genFunc();
+
+    const interval = setInterval(() => {
+      let val = genObj.next();
+      if (val.done) {
+        clearInterval(interval);
+      } else {
+        console.log(val.value);
+        //WORKER CALL GOES HERE
+        const passed = true;
+        setTestResults((prev) =>[...prev, passed]);
+      }
+    }, 1000);
   };
 
   return (
@@ -58,17 +80,29 @@ export default function CodeEditAndRun({ challenge }) {
         onBeforeChange={(editor, data, value) => setCode(value)}
       />
       <Button
-        onClick={calculate}
+        onClick={runTests}
         variant="contained"
         color="primary"
-        style={{ marginTop: 5, float: "right" }}
+        style={{ margin: 5, float: "right" }}
       >
-        Run
+        Run Test Cases
+      </Button>
+      <Button
+        onClick={evaluate}
+        variant="contained"
+        color="primary"
+        style={{ margin: 5, float: "right" }}
+      >
+        Evaluate
       </Button>
       <br />
       <span>Result: {result && JSON.stringify(result)}</span>
       <br />
       <span>Error: {error && JSON.stringify(error)}</span>
+      <br />
+      <span>Test Cases: {challenge.testcases && JSON.stringify(challenge.testcases)}</span>
+      <br />
+      <span>Test Results: {testResults && JSON.stringify(testResults)}</span>
       <iframe
         ref={iframeRef}
         title="hidden iframe"
