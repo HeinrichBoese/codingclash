54c0bdecbc902d15ecbfddd32e7166ae234e8a36
heinrich boese <heinrich.boese@googlemail.com>
2020-06-30 16:48:16 +0200

    auth aus sessionStorrage umgestellt

--- src/Auth.js
+++ src/Auth.js
@@ -4,8 +4,8 @@ import firebase from './firebase';
 export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem('AuthUser')));
-  const [userData, setUserData] = useState(JSON.parse(localStorage.getItem('UserData')));
+  const [currentUser, setCurrentUser] = useState(JSON.parse(sessionStorage.getItem('AuthUser')));
+  const [userData, setUserData] = useState(JSON.parse(sessionStorage.getItem('UserData')));
 
   useEffect(() => {
     const db = firebase.firestore();
@@ -13,19 +13,19 @@ export const AuthProvider = ({ children }) => {
       authUser => {
         setCurrentUser(authUser);
         if (authUser) {
-          localStorage.setItem('AuthUser', JSON.stringify(authUser));
+          sessionStorage.setItem('AuthUser', JSON.stringify(authUser));
         }
         if ((authUser && !userData) || (authUser && userData.email !== authUser.email)) {
           db.collection('User').doc(authUser.uid).get().then((doc) => {
             setUserData(doc.data());
-            localStorage.setItem('UserData', JSON.stringify(doc.data()))
+            sessionStorage.setItem('UserData', JSON.stringify(doc.data()))
           })
         }
       },
       // Error handling
       () => {
-        localStorage.removeItem('AuthUser');
-        localStorage.removeItem('UserData');
+        sessionStorage.removeItem('AuthUser');
+        sessionStorage.removeItem('UserData');
       },
     );
     return () => unsubscribe();
