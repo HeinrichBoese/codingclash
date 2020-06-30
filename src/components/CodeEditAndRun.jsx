18d4504a16c7d921e2ceaba47dc11d968506e068
daniel <danielschnietz@web.de>
2020-06-30 16:43:49 +0200

    aufgeräumt

--- src/components/CodeEditAndRun.jsx
+++ src/components/CodeEditAndRun.jsx
@@ -31,27 +31,6 @@ const useStyles = makeStyles((theme) => ({
         width:'100%',
        },
 
-
-    // display: "flex",
-    // flexWrap: "wrap",
-    // width: `calc(100vw - 170px)`,
-    // height: "calc(100vh - 122px)",
-    // justifyContent: "center",
-    // alignContent: "center",
-    // [theme.breakpoints.down("md")]: {
-    //   display: "block",
-    // },
-
-
-    // display: "block",
-    // width: "100vw",
-    // height: "100vh",
-    // [theme.breakpoints.up("sm")]: {
-    //   display: "flex",
-    //   flexWrap: "wrap",
-    //   width: "100vw",
-    //   height: "100vh",
-    // },
   },
   codeMirrContainer: {
 
@@ -76,14 +55,10 @@ const useStyles = makeStyles((theme) => ({
       height:'100%', 
       overflowY:'auto', 
       border:'none',
-      // borderRight:'none',
-      // borderTop:'none',
-      // borderBottom: 'none',
       boxSizing:'border-box',
        },
   },
   challengeDescContainer: {
-    // backgroundColor:'#2a2a2e', 
     width:'36%',
     height:'95%', 
     overflowY:'auto', 
@@ -100,9 +75,7 @@ const useStyles = makeStyles((theme) => ({
   },
   outputContainer: {
     width:'55% ',
-    // height:'43.3%',
     height:'calc(50% - 60px)',
-    // backgroundColor:'#252626',
     margin:0,
     border:'2px solid #f547e1',
     borderLeft: 'none',
@@ -111,19 +84,15 @@ const useStyles = makeStyles((theme) => ({
   },
   testResultsContainer: {
     width:'45%',
-    // height:'43.3%', 
     height:'calc(50% - 60px)',
     margin:0,
     border:'2px solid #f547e1',
     overflowY: 'auto',
     boxSizing:'border-box'
-    // backgroundColor:'grey'
   },
   actionsContainer:{
-    display:'flex', 
-    // flexWrap:'wrap', 
+    display:'flex',  
     width:'100%',
-    // height:'43.3%',
     height:'60px',
     margin: 0, 
     justifyContent:'center',
@@ -131,60 +100,6 @@ const useStyles = makeStyles((theme) => ({
     borderTop:'none',
     borderBottom: 'none',
     boxSizing:'border-box',
-
-
-  //   width: "57%",
-  //   height: "95%",
-  //   margin: 8,
-  //   display: "flex",
-  //   flexWrap: "wrap",
-  //   [theme.breakpoints.down("md")]: {
-  //     width: "100%",
-  //   },
-  // },
-  // challengeDescContainer: {
-  //   // backgroundColor:'#2a2a2e',
-  //   width: "40%",
-  //   height: "95%",
-  //   overflowY: "auto",
-  //   margin: 8,
-  //   border: "2px solid #f547e1",
-  //   [theme.breakpoints.down("md")]: {
-  //     width: "100%",
-  //   },
-  // },
-  // outputContainer: {
-  //   width: "44%",
-  //   height: "43.5%",
-  //   // backgroundColor:'#252626',
-  //   margin: 8,
-  //   marginLeft: 0,
-  //   marginRight: 0,
-  //   border: "2px solid #f547e1",
-  //   borderLeft: "none",
-  // },
-  // testResultsContainer: {
-  //   width: "37%",
-  //   height: "43.5%",
-  //   margin: 8,
-  //   marginLeft: 0,
-  //   marginRight: 0,
-  //   border: "2px solid #f547e1",
-  //   // backgroundColor:'grey'
-  // },
-  // actionsContainer: {
-  //   display: "flex",
-  //   flexWrap: "wrap",
-  //   width: "18%",
-  //   height: "43.5%",
-  //   margin: 8,
-  //   marginRight: 0,
-  //   marginLeft: 0,
-  //   justifyContent: "center",
-  //   border: "2px solid #f547e1",
-  //   borderLeft: "none",
-
-    // backgroundColor:'grey'
   },
 }));
 
@@ -347,202 +262,11 @@ export default function CodeEditAndRun({
           testPassed={testPassed}
           testRunning={testRunning}
           submitted={submitted}
-          // runTests={runTests}
-          // evaluate={evaluate}
-          // runButtonDisabled={runButtonDisabled}
-          // testButtonDisabled={testButtonDisabled}
         />
       </Box>
          <Box className={classes.outputContainer}> 
         <Output testcases={challenge.testcases} testError={testErrors} testResults={testResults} submitted={submitted} testRunning={testRunning} testPassed={testPassed}/>
          </Box>
-    {/* // <Container style={{ width:'100vw', height:'100vh'}}>
-=======
-    <div style={{ width: "calc(100vw-170)", height: "calc(100vh-123)" }}>
-      <Box className={classes.root}>
-        <Box className={classes.challengeDescContainer}>
-          <ChallengeDescription challenge={challenge} />
-        </Box>
-        <Box className={classes.codeMirrContainer}>
-          <Box style={{ width: "100%", height: "55%" }}>
-            <CodeMirror
-              value={code}
-              options={{
-                mode: "javascript",
-                theme: "shadowfox",
-                tabSize: 2,
-                lineNumbers: true,
-                screenReaderLabel: "code editor",
-              }}
-              onBeforeChange={(editor, data, value) => setCode(value)}
-            />
-            <iframe
-              ref={iframeRef}
-              title="hidden iframe"
-              style={{ display: "none" }}
-              sandbox="allow-scripts allow-same-origin"
-            />
-          </Box>
-          <Box className={classes.testResultsContainer}>
-            <TestResults
-              testcases={challenge.testcases}
-              testResults={testResults}
-              testError={testErrors}
-              testPassed={testPassed}
-              testRunning={testRunning}
-              submitted={submitted}
-              // runTests={runTests}
-              // evaluate={evaluate}
-              // runButtonDisabled={runButtonDisabled}
-              // testButtonDisabled={testButtonDisabled}
-            />
-          </Box>
-          <Box className={classes.outputContainer}>
-            <Output
-              testcases={challenge.testcases}
-              testError={testErrors}
-              testResults={testResults}
-              submitted={submitted}
-              testRunning={testRunning}
-              testPassed={testPassed}
-            />
-          </Box>
-          <Box className={classes.actionsContainer}>
-            <Actions
-              runTests={runTests}
-              evaluate={evaluate}
-              runButtonDisabled={runButtonDisabled}
-              testButtonDisabled={testButtonDisabled}
-              submit={submit}
-              allChecksDone={allChecksDone}
-            />
-          </Box>
-
-          {/* // <Container style={{ width:'100vw', height:'100vh'}}>
-
-    <div>
-      <Sidebar /> */}
-          {/* <Box style = {{display:'flex', flexWrap:'wrap', width:'100vw', height:'100vh'}}>
-      <Box className={classes.root}> */}
-          {/* <UserGameMenu secondsLeft={secondsLeft}/>
-        <Box display="flex" css={{ justifyContent: "center" }}>
-          <Typography style={{ color: "white", fontSize: 22 }}>
-            You have {secondsLeft}seconds left...
-          </Typography>
-        </Box>
-        <div
-          style={{
-            backgroundColor: "#2a2a2e",
-            width: "35vw",
-            height: "53vh",
-            overflowY: "auto",
-            margin: 8,
-          }}
-        >
-          <ChallengeDescription challenge={challenge} />
-        </div>
-        <Box style={{ width: "40vw", height: "53vh", margin: 8 }}>
-          <CodeMirror
-            value={code}
-            options={{
-              mode: "javascript",
-              theme: "shadowfox",
-              tabSize: 2,
-              lineNumbers: true,
-              screenReaderLabel: "code editor",
-            }}
-            onBeforeChange={(editor, data, value) => setCode(value)}
-          />
-
-          {/* <span>
-          Evaluation Result:{" "}
-          {result ? JSON.stringify(result) : "no return value"}
-        </span>
-        <br />
-        <span>
-          Error: {error ? JSON.stringify(error) : "no errors thrown :)"}
-        </span>
-        <br /> */}
-
-          {/* <Button
-          onClick={runTests}
-          disabled={testButtonDisabled}
-          variant="contained"
-          color="primary"
-          style={{ float: "right" }}
-        >
-          Run Test Cases
-        </Button>
-        <Button
-          onClick={evaluate}
-          disabled={runButtonDisabled}
-          variant="contained"
-          color="primary"
-          style={{  marginRight: 5, float: "right" }}
-        >
-          Evaluate
-        </Button> */}
-          {/* <br /> */}
-
-          {/* <iframe
-            ref={iframeRef}
-            title="hidden iframe"
-            style={{ display: "none" }}
-            sandbox="allow-scripts allow-same-origin"
-          />
-        </Box>
-        <Box
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            height: "37vh",
-            margin: 8,
-          }}
-        > */}
-          {/* <div>
-        <Playerboard players={players}/>
-      </div> */}
-          {/* <div>
-            <Box
-              style={{
-                width: "26vw",
-                height: "38vh",
-                backgroundColor: "#252626",
-                margin: 8,
-              }}
-            >
-              <Output
-                testcases={challenge.testcases}
-                testError={testErrors}
-                testResults={testResults}
-                submitted={submitted}
-                testRunning={testRunning}
-                testPassed={testPassed}
-              />
-            </Box>
-          </div>
-          <div>
-            <TestResults
-              testcases={challenge.testcases}
-              testResults={testResults}
-              testError={testErrors}
-              testPassed={testPassed}
-              testRunning={testRunning}
-              submitted={submitted} */}
-          {/* // runTests={runTests}
-              // evaluate={evaluate}
-              // runButtonDisabled={runButtonDisabled}
-              // testButtonDisabled={testButtonDisabled}
-            /> */}
-          {/* </div> */}
-          {/* <Actions
-            runTests={runTests}
-            evaluate={evaluate}
-            runButtonDisabled={runButtonDisabled}
-            testButtonDisabled={testButtonDisabled}
-            submit={submit}
-            allChecksDone={allChecksDone}
-          /> */}
         </Box>
       </Box>
     </div>
