446e7434290035fbeb4705d4704624e698f32834
daniel <danielschnietz@web.de>
2020-06-30 23:26:07 +0200

    lobby angepasst

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -1,23 +1,28 @@
 import React, { useRef } from "react";
 import Button from "@material-ui/core/Button";
-
+import { makeStyles } from '@material-ui/core/styles';
 // const Lobby = ({startGame, gameSessionID, players}) => {
   // const [gameID, setGameID] = useState("a0s8df9as8d7f");
   // const [players, setPlayers] = useState(["ich", "nr2", "nr3"]);
   // const { height, width } = useWindowDimensions();
 
-const Lobby = ({startGame, isLobbyLeader, leaveLobby}) => {
-
-  const styles = {
+  const useStyles = makeStyles((theme) => ({
     root: {
       // 
         // background: 'rgb(241,26,255)',
-        background:  "#2a2a2e",
+        // background:  "#2a2a2e",
         // background: 'linear-gradient(20deg, rgba(241,26,255,1) 50%, rgba(30,250,255,1) 100%)', 
         color: "#f547e1",
         fontSize:'1em',
         fontWeight:'bold',
         height: 50,
+        boxShadow: '0px 0px 20px 2px #00bef7',
+      transition: 'box-shadow .3s',
+      textShadow:'0px 0px 20px #f547e1',
+      // boxShadow: "0px 0px 20px 5px rgb(241,26,255)",
+      '&:hover': {
+        boxShadow: '0px 0px 20px 10px #00bef7',
+      },
         // padding: "0 30px",
         // boxShadow: "0 0px 5px 0px rgba(255, 105, 135, .3)",
         width:200,
@@ -31,13 +36,19 @@ const Lobby = ({startGame, isLobbyLeader, leaveLobby}) => {
       borderRadius: 0,
       borderTopRightRadius: 5,
       borderBottomRightRadius: 5,
-      backgroundColor: "#2a2a2e",
+      boxShadow: '0px 0px 20px 2px #00bef7',
+      transition: 'box-shadow .3s',
+      textShadow:'0px 0px 20px #f547e1',
+      '&:hover': {
+        boxShadow: '0px 0px 20px 10px #00bef7',
+      },
+      // backgroundColor: "#2a2a2e",
       border: '2px solid  #00bef7',
       width:100,
       height:40
     },
     textInput: {
-      backgroundColor: "#2a2a2e",
+      // backgroundColor: "#2a2a2e",
       color:'#f547e1',
       border: '2px solid  #00bef7',
       fontSize: 18,
@@ -59,7 +70,12 @@ const Lobby = ({startGame, isLobbyLeader, leaveLobby}) => {
     container: {
       alignItems: "center",
     },
-  };
+  }));
+
+
+const Lobby = ({startGame, isLobbyLeader, leaveLobby}) => {
+
+  const classes = useStyles();
 
   const textAreaRef = useRef(null);
   function copyToClipboard(e) {
@@ -73,25 +89,25 @@ const Lobby = ({startGame, isLobbyLeader, leaveLobby}) => {
 
 
   return (
-    <div style={styles.container}>
-      <form style={styles.form}>
-        <input style={styles.textInput} ref={textAreaRef} value={window.location.href} readOnly />
+    <div className={classes.container}>
+      <form className={classes.form}>
+        <input className={classes.textInput} ref={textAreaRef} value={window.location.href} readOnly />
         {document.queryCommandSupported("copy") && (
           <span>
-            <Button className="illuminate" style={styles.copy} onClick={copyToClipboard}>
+            <Button className={classes.copy} onClick={copyToClipboard}>
               Copy Link
             </Button>
           </span>
         )}
       </form>
 
-      <div style={styles.bottomButton}>
+      <div className={classes.bottomButton}>
 
         {/* // {isLobbyLeader && <Button className="illuminate" style={styles.root} onClick={startGame}>Start Game</Button>}
         // <Button className="illuminate" style={styles.root} component={Link} to={"/"}> */}
 
-        {isLobbyLeader() && <Button className="illuminate" style={styles.root} onClick={startGame}>Start Game</Button>}
-        <Button className="illuminate" onClick={leaveLobby} style={styles.root}>
+        {isLobbyLeader() && <Button className="illuminate" className={classes.root} onClick={startGame}>Start Game</Button>}
+        <Button className="illuminate" onClick={leaveLobby} className={classes.root}>
           Leave Lobby
         </Button>
       </div>
